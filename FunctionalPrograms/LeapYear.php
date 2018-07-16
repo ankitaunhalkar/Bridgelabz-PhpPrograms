5275b999345ba2e39af95ce6027a0c0484d04ae1
root <ankitaunhalkar19@gmail.com>
2018-07-16 19:25:22 +0530

    Functional

--- FunctionalPrograms/LeapYear.php
+++ FunctionalPrograms/LeapYear.php
@@ -5,17 +5,18 @@ include "Utility.php";
 echo "Enter the Year:";
 
 $year = inputInt();
+try {
+    $result = isLeap($year);
 
-$result = isLeap($year);
+    if ($result == true) {
+        echo "$year is a leap year\n";
+    } else {
+        echo "$year is not a leap year\n";
+    }
+} catch (LengthException $e) {
+    
+    echo "Message:" . $e->getMessage();
+    error_log($e->getMessage() . "\n", 3, "/var/www/html/error.log");
+    echo "\n";
 
-if($result==true)
-{
-    echo "$year is a leap year\n";
 }
-else
-{
-    echo "$year is not a leap year\n";
-}
-
-
-?>
--- FunctionalPrograms/Utility.php
+++ FunctionalPrograms/Utility.php
@@ -11,8 +11,9 @@ function inputString()
 function inputInt()
 {
     fscanf(STDIN, "%d\n", $str);
+    // $word= strchr($str,".");
+    // echo "double value" .$word;
     return $str;
-
 }
 
 #Float Input Method
@@ -22,15 +23,15 @@ function inputFloat()
     return $num;
 }
 
-#Array input of Int  
+#Array input of Int
 function inputArray($n)
 {
     $arr = [];
-   
-    for ($i=0; $i < $n; $i++) { 
-        
+
+    for ($i = 0; $i < $n; $i++) {
+
         $arr[$i] = inputInt();
-       
+
     }
 
     return $arr;
@@ -39,42 +40,44 @@ function inputArray($n)
 #Method to check Leap Year
 function isLeap($year)
 {
-    if($year%4==0 || $year%400==0 && $year%100!=0)
+    if (strlen($year)==4) 
     {
-        return true;
+        if ($year % 4 == 0 || $year % 400 == 0 && $year % 100 != 0) {
+            return true;
+        } else {
+            return false;
+        }
     }
-    else
-    {
-        return false;
+    else{
+        
+        throw new LengthException("Length should be equal to 4");
+        
     }
+
 }
 
 #Method to find harmonic value
 function harmonicValue($num)
 {
     $harmonicValue = 0;
-    
-        for ($i=1; $i <= $num; $i++) { 
-            $harmonicValue += 1/$i;      
-        }
-    
+
+    for ($i = 1; $i <= $num; $i++) {
+        $harmonicValue += 1 / $i;
+    }
+
     return $harmonicValue;
 }
 
 #Method to find nth power of 2
 function powerOf2($num)
 {
-    if($num>=0 && $num<=31)
-    {
-        $value=1;
-            for($i=0;$i<=$num;$i++)
-            {
-                echo "2^$i = $value \n";
-                $value = 2*$value;
-            }
-    }
-    else
-    {
+    if ($num >= 0 && $num <= 31) {
+        $value = 1;
+        for ($i = 0; $i <= $num; $i++) {
+            echo "2^$i = $value \n";
+            $value = 2 * $value;
+        }
+    } else {
         echo "Enter Number greater then 0 and less then 31";
     }
 }
@@ -82,47 +85,41 @@ function powerOf2($num)
 #Method to find prime factor
 function primeFactor($num)
 {
-    for($i=2;$i*$i<=$num;$i++)
-    {
-        while( $num % $i == 0 )
-        {
+    for ($i = 2; $i * $i <= $num; $i++) {
+        while ($num % $i == 0) {
             echo "$i ";
-            $num = $num/$i; 
-        }   
+            $num = $num / $i;
+        }
     }
-    if($num>1)
+    if ($num > 1) {
         echo "$num \n";
-    else
+    } else {
         echo "\n";
+    }
+
 }
 
 #Method for gamblers
 function gambler($stake, $goal, $times)
 {
-    $win=0;
-   
-    for($i=1;$i<=$times;$i++)
-    {   
-        while($stake>0 && $stake<$goal)
-        {
-            if(mt_rand(0,1)<0.5)
-            {
+    $win = 0;
+
+    for ($i = 1; $i <= $times; $i++) {
+        while ($stake > 0 && $stake < $goal) {
+            if (mt_rand(0, 1) < 0.5) {
                 $stake++;
-            }
-            else
-            {
+            } else {
                 $stake--;
             }
-            if($stake==$goal)
-            {
+            if ($stake == $goal) {
                 $win++;
             }
         }
     }
-    echo "Times won:",$win;
-    $winPercent = $win*100/$times;
-    echo "\nPercent of win:",$winPercent;
-    echo "\nPercent of loss:",(($times-$win)*100/$times);
+    echo "Times won:", $win;
+    $winPercent = $win * 100 / $times;
+    echo "\nPercent of win:", $winPercent;
+    echo "\nPercent of loss:", (($times - $win) * 100 / $times);
 }
 
 #Utility Class
@@ -130,26 +127,28 @@ class Utility
 {
 
     #Method to generate random number
-    public static function random(){
-        $random = mt_rand(1000,9999);
+    public static function random()
+    {
+        $random = mt_rand(1000, 9999);
         return $random;
     }
 
     #Method to generate distinct coupon number
-    public static function couponNumber($n){
+    public static function couponNumber($n)
+    {
         $flag = false;
         $distinct = array($n);
-        for ($i=0; $i < $n; $i++) {
+        for ($i = 0; $i < $n; $i++) {
             $randomValue = Utility::random();
             echo "$randomValue\n";
-            for ($j=0; $j < $n; $j++) { 
-                
+            for ($j = 0; $j < $n; $j++) {
+
                 if ($distinct[$j] == $randomValue) {
 
                     break;
 
                 }
-                if (!$flag){
+                if (!$flag) {
 
                     $distinct[$i] = $randomValue;
 
@@ -157,99 +156,85 @@ class Utility
             }
         }
 
-      foreach ($distinct as $value) {
-         echo "$value ";
-      }
+        foreach ($distinct as $value) {
+            echo "$value ";
+        }
     }
 
     #Method to print 2D Array
     public static function printArray($arr)
     {
-            foreach ($arr as $row) 
-            {
-                foreach ($row as $value) 
-                {
-                    echo $value." ";
-                }
-                    echo "\n";         
+        foreach ($arr as $row) {
+            foreach ($row as $value) {
+                echo $value . " ";
             }
+            echo "\n";
+        }
     }
 
     #Method for 2D Array
-    public static function array2D($m,$n,$choice)
+    public static function array2D($m, $n, $choice)
     {
-        switch($choice)
-        {
-            case 1: $arr = [[]];
-                    echo "Enter elemnts into Matrix:\n";
-                    for($i=0;$i<$m;$i++)
-                    {
-                        for($j=0;$j<$n;$j++)
-                        {
-                            $arr[$i][$j] = inputInt();
-                        }
+        switch ($choice) {
+            case 1:$arr = [[]];
+                echo "Enter elemnts into Matrix:\n";
+                for ($i = 0; $i < $m; $i++) {
+                    for ($j = 0; $j < $n; $j++) {
+                        $arr[$i][$j] = inputInt();
                     }
+                }
 
-                    echo "Your Matrix is:\n";
-        
-                    Utility :: printArray($arr);                
+                echo "Your Matrix is:\n";
 
-                    break;
-                    
+                Utility::printArray($arr);
 
-            case 2: $arr = [[]];
-                    echo "Enter elemnts into Matrix:\n";
-                    for($i=0;$i<$m;$i++)
-                    {
-                        for($j=0;$j<$n;$j++)
-                        {
-                            $arr[$i][$j] = inputFloat();
-                        }
+                break;
+
+            case 2:$arr = [[]];
+                echo "Enter elemnts into Matrix:\n";
+                for ($i = 0; $i < $m; $i++) {
+                    for ($j = 0; $j < $n; $j++) {
+                        $arr[$i][$j] = inputFloat();
                     }
-                    echo "Your Matrix is:\n";
-                
-                    Utility :: printArray($arr);
+                }
+                echo "Your Matrix is:\n";
 
-                    break;
+                Utility::printArray($arr);
 
-            case 3: $arr = [[]];
-                    echo "Enter elemnts into Matrix:\n";
-                    for($i=0;$i<$m;$i++)
-                    {
-                        for($j=0;$j<$n;$j++)
-                        {
-                            $arr[$i][$j] = inputString();
-                        }
+                break;
+
+            case 3:$arr = [[]];
+                echo "Enter elemnts into Matrix:\n";
+                for ($i = 0; $i < $m; $i++) {
+                    for ($j = 0; $j < $n; $j++) {
+                        $arr[$i][$j] = inputString();
                     }
-                    echo "Your Matrix is:\n";
-                   
-                    Utility :: printArray($arr);
+                }
+                echo "Your Matrix is:\n";
 
-                    break;
+                Utility::printArray($arr);
 
-            default: break;
+                break;
+
+            default:break;
         }
     }
 
     #Sum Of three Integer equals to zero
     public static function triplet($arr)
     {
-       $count = 0;
-       for ($i=0; $i < count($arr); $i++) 
-       { 
-           for ($j=$i+1; $j < count($arr); $j++) 
-           { 
-               for ($k=$j+1; $k < count($arr); $k++) 
-               { 
-                   if($arr[$i]+$arr[$j]+$arr[$k] == 0)
-                   {
+        $count = 0;
+        for ($i = 0; $i < count($arr); $i++) {
+            for ($j = $i + 1; $j < count($arr); $j++) {
+                for ($k = $j + 1; $k < count($arr); $k++) {
+                    if ($arr[$i] + $arr[$j] + $arr[$k] == 0) {
                         $count++;
-                        echo "\nNumber of Triplet: ",$count;
-                        echo "\nTrpilet: [".$arr[$i].",".$arr[$j].",".$arr[$k]."]";
-                   }
-               }
-           }
-       }
+                        echo "\nNumber of Triplet: ", $count;
+                        echo "\nTrpilet: [" . $arr[$i] . "," . $arr[$j] . "," . $arr[$k] . "]";
+                    }
+                }
+            }
+        }
     }
 
     #Method to Find Distance
@@ -261,7 +246,7 @@ class Utility
         //distance = sqrt(x*x + y*y)
         //√(x2  – x1)² + (y2 – y1)²
 
-        $distance = sqrt(pow($x1 - $x,2) + pow($y1 - $y,2));
+        $distance = sqrt(pow($x1 - $x, 2) + pow($y1 - $y, 2));
 
         return $distance;
     }
@@ -270,30 +255,28 @@ class Utility
     public static function stopWatch($start, $end)
     {
         $timeLapsed = ($end - $start);
-        echo "\nTime elapsed is:",$timeLapsed."\n";
+        echo "\nTime elapsed is:", $timeLapsed . "\n";
     }
 
     #Method for Quadratic
-    public static function quadratic($a,$b,$c)
+    public static function quadratic($a, $b, $c)
     {
-        $delta=($b*$b) - (4*$a*$c);
+        $delta = ($b * $b) - (4 * $a * $c);
         echo $delta;
-        if($delta!=0)
-        {
-            $dsq=sqrt($delta);
+        if ($delta != 0) {
+            $dsq = sqrt($delta);
             //echo $dsq;
-            $x1=(-$b + $dsq)/(2*$a);
-            $x2=(-$b - $dsq)/(2*$a);
-            echo "\nRoots of x is: (".$x1.",".$x2.")\n";
+            $x1 = (-$b + $dsq) / (2 * $a);
+            $x2 = (-$b - $dsq) / (2 * $a);
+            echo "\nRoots of x is: (" . $x1 . "," . $x2 . ")\n";
         }
     }
 
     #Method for Windmill
     public static function windMill($t, $v)
     {
-        $vpow=pow($v, 0.16);
-        $w=(35.74+0.6215*$t+(0.4275*$t-35.75)*$vpow);
-        echo "\nWindmill:",$w."\n";
+        $vpow = pow($v, 0.16);
+        $w = (35.74 + 0.6215 * $t + (0.4275 * $t - 35.75) * $vpow);
+        echo "\nWindmill:", $w . "\n";
     }
 }
-?>
